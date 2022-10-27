4d0a6d115ae27ee2e23c0c5e8612adc5c1866537
IliasOmari <ilias.omari@student.ehb.be>
2022-10-27 20:26:45 +0200

    test of my api with a fetch

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict"
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '78cd515f38msh0b9626d040e5cc3p137289jsn92ebfe031371',
+        'X-RapidAPI-Host': 'binance-nft.p.rapidapi.com'
+    }
+};
+
+fetch('https://binance-nft.p.rapidapi.com/top-collections/?day=0', options)
+    .then(response => response.json())
+    .then(response => console.log(response.data.forEach(element => {
+        
+    })))
