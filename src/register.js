d4043da06dd77bf07dd60b0629b08a2671fae2c0
IliasOmari <ilias.omari@student.ehb.be>
2022-12-05 18:33:33 +0100

    inserted a second password for check

--- src/register.js
+++ src/register.js
@@ -12,38 +12,47 @@ rgstBtn.addEventListener('click', (e) => {
     const username = document.getElementById('inputUsername').value
     const email = document.getElementById('inputEmail').value
     const password = document.getElementById('inputPassword').value
+    const password2 = document.getElementById('inputPassword2').value
 
-    console.log(email, password, username)
+    console.log(email, password, password2, username)
 
-    fetch("http://localhost:1200/register", {
-            method: "POST",
-            headers: {
-                'Content-Type': "application/json"
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                username: username
+    if (password == password2) {
+
+        fetch("http://localhost:1200/register", {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    email: email,
+                    password: password,
+                    password2: password2
+                })
+
+
             })
 
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-
-            if (data.message == "Your account has been successfully created") {
-                alert(data.message)
-                window.location.href = "login.html"
-
-            } else if (data.message == "Missing username, mail, password") {
-                alert(data.message)
-
-            } else if (data.message == "This email is already used") {
-                alert(data.message)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
 
 
-            }
-        })
+                if (data.message == "Your account has been successfully created") {
+                    alert(data.message)
+                    window.location.href = "login.html"
 
+                } else if (data.message == "Missing username, mail, password") {
+                    alert(data.message)
+
+                } else if (data.message == "This email is already used") {
+                    alert(data.message)
+
+
+                }
+            })
+    } else {
+        alert("you did not enter the same passwords. Please try again")
+    }
 
 })
