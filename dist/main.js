4525669f20842a3511940cb7444a6163e92265e1
IliasOmari <ilias.omari@student.ehb.be>
2022-12-16 16:59:56 +0100

    configured webpack part2

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/script.js":
-/*!***********************!*\
-  !*** ./src/script.js ***!
-  \***********************/
-/***/ (() => {
-
-eval("console.timeLog('hello')\n\n//# sourceURL=webpack://web-2-frontend-22-23-iliasomari/./src/script.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/script.js"]();
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/script.js',
+  entry: {
+    auth: './src/auth.js',
+    collection: './src/collection.js',
+    creators: './src/creators.js',
+    like: './src/like.js',
+    login: './src/login.js',
+    nft: './src/nft.js',
+    register: './src/register.js',
+    username: './src/username.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  mode: 'production',
   watch: true
-};
+}
