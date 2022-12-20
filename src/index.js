fa9df212f740cebdfca3dc6b0d4816e4a0e7c1cd
IliasOmari <ilias.omari@student.ehb.be>
2022-12-20 18:25:47 +0100

    added my red heart

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const user = sessionStorage.getItem('user')
 if (user) {
     const parsed = JSON.parse(sessionStorage.getItem('user'))
     document.getElementById('welcomemessage').textContent = `Welcome to the NFT Universe ${parsed.username}!`
-    
+
 }
